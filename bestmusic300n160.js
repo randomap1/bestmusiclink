a02312e7fd238c6f93c9743f5cb3a6852aba81ba
randomap1 <118092966+randomap1@users.noreply.github.com>
2022-12-16 01:26:34 +0700

    Create bestmusic300n160.js

---
+++ bestmusic300n160.js
@@ -0,0 +1,8 @@
+	atOptions = {
+		'key' : '1bc7ef7115c45cf3186dab9a15d9973c',
+		'format' : 'iframe',
+		'height' : 300,
+		'width' : 160,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://capricedes.com/1bc7ef7115c45cf3186dab9a15d9973c/invoke.js"></scr' + 'ipt>');
