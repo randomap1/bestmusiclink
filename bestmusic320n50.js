22f4b32da5f04e8e2fef3efe086a666bb547ce2c
randomap1 <118092966+randomap1@users.noreply.github.com>
2022-12-15 15:29:43 +0700

    Create bestmusic320n50.js

---
+++ bestmusic320n50.js
@@ -0,0 +1,8 @@
+	atOptions = {
+		'key' : 'a031268eea304558ee7b2dceeac6ab17',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://capricedes.com/a031268eea304558ee7b2dceeac6ab17/invoke.js"></scr' + 'ipt>');
